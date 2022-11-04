a8d6bc357fdc81104c62257f003bdf793568ea78
harry lachenmayer <harrylachenmayer@gmail.com>
2022-11-04 22:52:39 +0000

    shmap

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use core::panic;
+use std::{
+    env, io,
+    process::{Command, ExitStatus},
+};
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if let [_, var_name, command] = &args[..] {
+        let stdin = io::stdin();
+        for maybe_line in stdin.lines() {
+            let line = maybe_line?;
+            spawn_with_env(command, var_name, line)?;
+        }
+    } else {
+        panic!("Usage: shmap var_name 'command containing $var_name'");
+    }
+    Ok(())
+}
+
+fn spawn_with_env(
+    command: &String,
+    var_name: &String,
+    value: String,
+) -> Result<ExitStatus, std::io::Error> {
+    Command::new("/bin/sh")
+        .arg("-c")
+        .arg(command)
+        .env(var_name, value)
+        .status()
+}
